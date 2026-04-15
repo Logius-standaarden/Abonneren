7a58725b654b5da0e5665e2e309b93bc5e0ebebf
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-04-15 09:42:16 +0200

    Beheer: fix Logius links

--- js/config.mjs
+++ js/config.mjs
@@ -19,17 +19,17 @@ loadRespecWithConfiguration({
   editors:
     [
       {
-        name: "Logius Standaarden",
+        name: "Stas Mironov",
         company: "Logius",
-        companyURL: "https://logius.nl",
+        companyURL: "https://www.logius.nl",
       }
     ],
   authors:
     [
       {
-        name: "Logius Standaarden",
+        name: "Stas Mironov",
         company: "Logius",
-        companyURL: "https://logius.nl",
+        companyURL: "https://www.logius.nl",
       }
     ],
   postProcess: [ generateMermaidFigures ],
